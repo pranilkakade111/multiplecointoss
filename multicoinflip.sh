8b98a43c9d4e36580df0a6d90bda22e87d01bb8e
Pranil Kakade <pranilkakade111@gmail.com>
2021-01-22 02:30:12 +0530

    Flipping multiple coin using loop

--- multicoinflip.sh
+++ multicoinflip.sh
@@ -7,3 +7,38 @@ then
 else
     echo "it is tales"
 fi
+
+function coinFlip() {
+temp=$1
+hwin=0
+twin=0
+for (( i=0 ; $i < $temp ; i++ ))
+do
+rand=$(($RANDOM%2))
+
+if [ $rand  -eq 1 ]
+then 
+	echo "Head Wins"
+	z='heads'
+	hwin=$(( $hwin + 1 ))
+	arr[i]=$hwin
+else
+	echo "Tails wins"
+	s='tails'
+	twin=$(( $twin + 1 ))
+	arr[i]=$twin
+fi
+done
+echo $hwin
+x=$(($hwin/$temp))
+Hpercentage=$(( $x * 100 ))
+echo $Hpercentage
+echo $twin
+y=$(($twin/$temp))
+Tpercentage=$(( $y * 100 ))
+for KEY in "${!arr[@]}"; 
+do
+	echo $KEY
+  echo "Value: ${arr[$KEY]}"
+done
+}
